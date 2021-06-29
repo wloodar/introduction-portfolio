28f6b9290a6e9fe51ea00be51840ec327e4c5318
Wlodar <wlodardev@gmail.com>
2021-06-29 18:49:08 +0200

    Move code to repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import s from './index.module.scss';
+
+function Home() {
+    return (
+        <div className={s.box}>
+            <h1>This will be a portfolio :)</h1>
+        </div>
+    )
+}
+
+export default Home;
