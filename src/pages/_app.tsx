b7783a5bf78106caf3ab9ab4d7526a7e7a52b3c5
Wlodar <wlodardev@gmail.com>
2021-07-01 23:59:38 +0200

    Loading bar on route change added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import { AnimateSharedLayout } from 'framer-motion';
 import type { AppProps } from 'next/app'
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import '../../public/styles/app.css';
+
+NProgress.configure({
+  showSpinner: false
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
