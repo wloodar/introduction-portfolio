28644be4d8e01ea1ef28eb244a3248e3637876d0
Wlodar <wlodardev@gmail.com>
2021-09-09 01:30:51 +0200

    Responsive nav list animation

--- src/common/components/Navbar/Navbar.tsx
+++ src/common/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import $ from 'jquery';
 // @ts-ignore
 import { animateScroll } from 'react-scroll';
 import useTranslation from 'next-translate/useTranslation';
@@ -12,6 +13,7 @@ const Navbar = () => {
 
     const [navbarShadow, setNavbarShadow] = useState(false);
     const [navOpen, setNavOpen] = useState(false);
+    const navListRef = useRef(null);
     const { t } = useTranslation();
 
     const handleScroll = () => {
@@ -27,13 +29,38 @@ const Navbar = () => {
     }
 
     useEffect(() => {
-
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     });
 
     const toggleMenu = () => {
+        
+        if (!navOpen) {
+            $(navListRef.current).find("li").each(function(i: any) {
+                var el = $(this);
+
+                setTimeout(function(){
+                    el.animate({opacity: "1"}, { step: function() {
+                        $(this).css({transform: 'translateY(0)'})
+                    }}, {duration: 200, queue: false})
+                            .delay(0).animate({opacity: 1}, {duration: 200});
+                },i*50);
+            });
+        } else {
+            $(navListRef.current).find("li").each(function(i: any) {
+                var el = $(this);
+        
+                setTimeout(function(){
+                    el.animate({opacity: "0"}, {step: function() {
+                        $(this).css({transform: ''})
+                    }}, {duration: 200, queue: false})
+                            .delay(0).animate({opacity: 0}, {duration: 200});
+                },i*50);
+            });
+        }
+
         setNavOpen(!navOpen);
+        // console.log($(navListRef.current).children());
     }
 
     return (
@@ -47,7 +74,7 @@ const Navbar = () => {
                 <div className={s.mobileRight}>
                     <div className={s.mobileIcon}>
                         <label htmlFor="check">
-                            <input type="checkbox" id="check" onClick={() => setNavOpen(!navOpen)} checked={navOpen}/> 
+                            <input type="checkbox" id="check" onClick={() => toggleMenu()} checked={navOpen}/> 
                             <span></span>
                             <span></span>
                             <span></span>
@@ -56,7 +83,7 @@ const Navbar = () => {
                 </div>
                 <span className={s.mobile} style={ navOpen ? { visibility: 'visible', opacity: '1' } : undefined } onClick={() => toggleMenu()}>
                     <span className={s.mobileBox}>
-                        <div className={s.list}>
+                        <div className={s.list} ref={navListRef}>
                             <NavList/>
                         </div>
                         <div className={s.right}>
