f022cda00090a732f368bdd902d885f2951f10a2
wloodar <wlodardev@gmail.com>
2021-09-09 00:18:09 +0200

    Eslint ignore during builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextTranslate = require('next-translate')
 module.exports = {
     reactStrictMode: true,
 
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     // Translation setup
     ...nextTranslate(),
 }
